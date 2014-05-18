674c31abdf19e7610a0f164b0214b91a94c87a1d
David Jones <drj@pobox.com>
2014-05-18 09:44:12 +0100

    move GHCNV3Reader to ghcn.M.read()

--- code/ghcn.py
+++ code/ghcn.py
@@ -20,212 +20,221 @@ import re
 # Clear Climate Code
 import giss_data
 
-def GHCNV3Reader(path=None, file=None,
-  year_min=None,
-  MISSING=8888):
-    """Reads a file in GHCN V3 .dat format and yields each station
-    record (as a giss_data.Series instance).
+class M:
+    def read(path=None, file=None, year_min=None, MISSING=8888):
+        """Reads a file in GHCN V3 .dat format and yields each station
+        record (as a giss_data.Series instance).
 
-    Station metadata (location, and so on) is read from a file
-    with a .inv extension (this is read automatically), and is
-    used to populate the `station` attribute of the Series
-    instances that are yielded.
+        Station metadata (location, and so on) is read from a file
+        with a .inv extension (this is read automatically), and is
+        used to populate the `station` attribute of the Series
+        instances that are yielded.
 
-    If `year_min` is specified, then only years >= year_min are kept
-    (the default, None, keeps all years).
-    
-    The (integer) values in the file are scaled according to the
-    element type. So for TAVG the values stored in the file are
-    in units of 0.01 degrees C, but the returned values from
-    this function are in degrees C.
+        If `year_min` is specified, then only years >= year_min are kept
+        (the default, None, keeps all years).
+        
+        The (integer) values in the file are scaled according to the
+        element type. So for TAVG the values stored in the file are
+        in units of 0.01 degrees C, but the returned values from
+        this function are in degrees C.
 
-    See ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README for format
-    of this file.
-    """
+        See ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README for format
+        of this file.
+        """
 
-    # If path is supplied, open it to use an input.
-    if path:
-        inp = open(path)
-    else:
-        inp = file
-
-    # Derive meta file location from pathname.
-    if not path:
-        path = inp.name
-    v3meta_filename = re.sub(r'[.]dat$', '.inv', path)
-    meta = station_metadata(path=v3meta_filename, format='v3')
-
-    def id11(l):
-        """Extract the 11-digit station identifier."""
-        return l[:11]
-
-    noted_element = False
-    def note_element(element):
-        """Print the meteorological element we are reading."""
-        friendly = dict(TAVG='average temperature',
-          TMIN='mean minimum temperature',
-          TMAX='mean maximum temperature')
-        print "(Reading %s)" % friendly[element]
-
-    element_scale = dict(TAVG=0.01, TMIN=0.01, TMAX=0.01)
-    # Quality-control flags that cause value to be rejected.
-    # :todo: make parameter.  When using the QCA dataset
-    # we shouldn't actually see any of these flags.
-    reject = 'DKOSTW'
-
-    def convert(s):
-        """Convert single value. *s* is the 8 character string: 5
-        characters for value, 3 for flags."""
-
-        # This function captures *multiplier* which can, in principle,
-        # change for each line.
-
-        v = int(s[:5])
-        # Flags for Measurement (missing days), Quality, and
-        # Source.
-        m,q,s = s[5:8]
-        if q in reject or v == -9999:
-            v = MISSING
+        # If path is supplied, open it to use an input.
+        if path:
+            inp = open(path)
         else:
-            v *= multiplier
-        return v
+            inp = file
 
-    all_missing = [MISSING]*12
+        # Derive meta file location from pathname.
+        if not path:
+            path = inp.name
+        v3meta_filename = re.sub(r'[.]dat$', '.inv', path)
+        meta = M.station_metadata(path=v3meta_filename, format='v3')
 
-    for id,lines in itertools.groupby(inp, id11):
-        key = dict(uid=id, first_year=year_min)
-        if meta.get(id):
-            key['station'] = meta[id]
-        record = giss_data.Series(**key)
-        for line in lines:
-            year = int(line[11:15])
-            element = line[15:19]
-            if not noted_element:
-                note_element(element)
-                noted_element = True
-            multiplier = element_scale[element]
-            values = [convert(line[i:i+8]) for i in range(19,115,8)]
-            if values != all_missing:
-                record.add_year(year, values)
-        if len(record) != 0:
-            yield record
+        def id11(l):
+            """Extract the 11-digit station identifier."""
+            return l[:11]
 
-class GHCNV3Writer(object):
-    """Write a file in GHCN v3 format. See also GHCNV3Reader.  The
-    format is documented in
-    ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README .  If the records
-    have an 'element' property, then that is used for the 'element'
-    field in the GHCN V3 file, otherwise 'TAVG' is used.
-    """
+        noted_element = False
+        def note_element(element):
+            """Print the meteorological element we are reading."""
+            friendly = dict(TAVG='average temperature',
+              TMIN='mean minimum temperature',
+              TMAX='mean maximum temperature')
+            print "(Reading %s)" % friendly[element]
 
-    def __init__(self, path=None, file=None, MISSING=8888, scale=0.01, **k):
-        if path is not None:
-            self.f = open(path, "w")
-        else:
-            self.f = file
-        self.scale = scale
+        element_scale = dict(TAVG=0.01, TMIN=0.01, TMAX=0.01)
+        # Quality-control flags that cause value to be rejected.
+        # :todo: make parameter.  When using the QCA dataset
+        # we shouldn't actually see any of these flags.
+        reject = 'DKOSTW'
 
-    def to_text(self, t):
-        if t == MISSING:
-            return "-9999"
-        else:
-            return "%5d" % t
+        def convert(s):
+            """Convert single value. *s* is the 8 character string: 5
+            characters for value, 3 for flags."""
 
-    def write(self, record):
-        """Write an entire record out."""
-        for year in range(record.first_year, record.last_year + 1):
-            if not record.has_data_for_year(year):
-                continue
-            element = getattr(record, 'element', 'TAVG')
-            self.writeyear(record.uid, element, year, record.get_a_year(year))
+            # This function captures *multiplier* which can, in principle,
+            # change for each line.
 
-    def writeyear(self, uid, element, year, temps):
-        """Write a single year's worth of data out.  *temps* should
-        contain 12 monthly values."""
+            v = int(s[:5])
+            # Flags for Measurement (missing days), Quality, and
+            # Source.
+            m,q,s = s[5:8]
+            if q in reject or v == -9999:
+                v = MISSING
+            else:
+                v *= multiplier
+            return v
 
-        if len(uid) > 11:
-            # Convert GHCN v2 style identifier into 11-digit v3
-            # identifier; use 12th digit for the source flag.
-            assert len(uid) == 12
-            sflag = uid[11]
-        elif len(uid) == 6:
-            # Assume it's a 6 digit identifier from USHCN.
-            uid = '42500' + uid
-            sflag = 'U'
-        else:
-            sflag = ' '
-        id11 = "%-11.11s" % uid
-        assert len(element) == 4
+        all_missing = [MISSING]*12
 
-        tstrings = [self.to_text(t)
-                   for t in internal_to_external(temps, scale=self.scale)]
-        flags = ['  ' + sflag] * 12
+        for id,lines in itertools.groupby(inp, id11):
+            key = dict(uid=id, first_year=year_min)
+            if meta.get(id):
+                key['station'] = meta[id]
+            record = giss_data.Series(**key)
+            for line in lines:
+                year = int(line[11:15])
+                element = line[15:19]
+                if not noted_element:
+                    note_element(element)
+                    noted_element = True
+                multiplier = element_scale[element]
+                values = [convert(line[i:i+8]) for i in range(19,115,8)]
+                if values != all_missing:
+                    record.add_year(year, values)
+            if len(record) != 0:
+                yield record
 
-        self.f.write('%s%04d%s%s\n' % (id11, year, element,
-          ''.join(t+flag for t,flag in zip(tstrings,flags))))
+    class Writer(object):
+        """Write a file in GHCN v3 format. See also GHCNV3Reader.  The
+        format is documented in
+        ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README .  If the records
+        have an 'element' property, then that is used for the 'element'
+        field in the GHCN V3 file, otherwise 'TAVG' is used.
+        """
 
-    def close(self):
-        self.f.close()
+        def __init__(self, path=None, file=None, MISSING=8888, scale=0.01, **k):
+            if path is not None:
+                self.f = open(path, "w")
+            else:
+                self.f = file
+            self.scale = scale
+
+        def to_text(self, t):
+            if t == MISSING:
+                return "-9999"
+            else:
+                return "%5d" % t
+
+        def write(self, record):
+            """Write an entire record out."""
+            for year in range(record.first_year, record.last_year + 1):
+                if not record.has_data_for_year(year):
+                    continue
+                element = getattr(record, 'element', 'TAVG')
+                self.writeyear(record.uid, element, year, record.get_a_year(year))
+
+        def writeyear(self, uid, element, year, temps):
+            """Write a single year's worth of data out.  *temps* should
+            contain 12 monthly values."""
+
+            if len(uid) > 11:
+                # Convert GHCN v2 style identifier into 11-digit v3
+                # identifier; use 12th digit for the source flag.
+                assert len(uid) == 12
+                sflag = uid[11]
+            elif len(uid) == 6:
+                # Assume it's a 6 digit identifier from USHCN.
+                uid = '42500' + uid
+                sflag = 'U'
+            else:
+                sflag = ' '
+            id11 = "%-11.11s" % uid
+            assert len(element) == 4
+
+            tstrings = [self.to_text(t)
+                       for t in internal_to_external(temps, scale=self.scale)]
+            flags = ['  ' + sflag] * 12
+
+            self.f.write('%s%04d%s%s\n' % (id11, year, element,
+              ''.join(t+flag for t,flag in zip(tstrings,flags))))
+
+        def close(self):
+            self.f.close()
 
 
-def station_metadata(path=None, file=None, format='v3'):
-    """Read station metadata from file, return it as a dictionary.
-    *format* specifies the format of the metadata; it can only be
-    'v3' (for GHCN-M v3). It exists to provide compatibility
-    with an alternate implementation of the same interface.
-    """
+    def station_metadata(path=None, file=None, format='v3'):
+        """Read station metadata from file, return it as a dictionary.
+        *format* specifies the format of the metadata; it can only be
+        'v3' (for GHCN-M v3). It exists to provide compatibility
+        with an alternate implementation of the same interface.
+        """
 
-    # Do not supply both arguments!
-    assert not (file and path)
+        # Do not supply both arguments!
+        assert not (file and path)
 
-    if path:
-        file = open(path)
-    assert file
+        if path:
+            file = open(path)
+        assert file
 
-    assert 'v3' == format
+        assert 'v3' == format
 
-    # With the beta GHCN V3 metadata, several fields are blank for some
-    # stations.  When processed as ints, these will get converted to
-    # None."""
-    def blank_int(s):
-        """Convert a field to an int, or if it is blank, convert to
-        None."""
+        # With the beta GHCN V3 metadata, several fields are blank for some
+        # stations.  When processed as ints, these will get converted to
+        # None."""
+        def blank_int(s):
+            """Convert a field to an int, or if it is blank, convert to
+            None."""
 
-        if s.isspace():
-            return None
-        return int(s)
+            if s.isspace():
+                return None
+            return int(s)
 
-    # Fields are named after the designators used in the GHCN v3
-    # documentation except for:
-    # uid (GHCN: ID), lat (GHCN: latitude), lon (GHCN: longitude).
+        # Fields are named after the designators used in the GHCN v3
+        # documentation except for:
+        # uid (GHCN: ID), lat (GHCN: latitude), lon (GHCN: longitude).
 
-    # See ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README for format
-    # of GHCN's metadata file.
-    v3_ghcn_fields = dict(
-        uid=    (0,    11, str),
-        lat=    (12,   20, float),
-        lon=    (21,   30, float),
-        stelev= (31,   37, float),
-        name=   (38,   68, str),
-        grelev= (69,   73, blank_int),
-        popcls= (73,   74, str),
-        popsiz= (75,   79, blank_int),
-        topo=   (79,   81, str),
-        stveg=  (81,   83, str),
-        stloc=  (83,   85, str),
-        ocndis= (85,   87, blank_int),
-        airstn= (87,   88, str),
-        towndis=(88,   90, blank_int),
-        grveg=  (90,  106, str),
-        popcss= (106, 107, str),
-    )
+        # See ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/README for format
+        # of GHCN's metadata file.
+        v3_ghcn_fields = dict(
+            uid=    (0,    11, str),
+            lat=    (12,   20, float),
+            lon=    (21,   30, float),
+            stelev= (31,   37, float),
+            name=   (38,   68, str),
+            grelev= (69,   73, blank_int),
+            popcls= (73,   74, str),
+            popsiz= (75,   79, blank_int),
+            topo=   (79,   81, str),
+            stveg=  (81,   83, str),
+            stloc=  (83,   85, str),
+            ocndis= (85,   87, blank_int),
+            airstn= (87,   88, str),
+            towndis=(88,   90, blank_int),
+            grveg=  (90,  106, str),
+            popcss= (106, 107, str),
+        )
 
-    fields = v3_ghcn_fields
+        fields = v3_ghcn_fields
 
-    result = {}
-    for line in file:
-        d = dict((field, convert(line[a:b]))
-                  for field, (a,b,convert) in fields.items())
-        result[d['uid']] = giss_data.Station(**d)
+        result = {}
+        for line in file:
+            d = dict((field, convert(line[a:b]))
+                      for field, (a,b,convert) in fields.items())
+            result[d['uid']] = giss_data.Station(**d)
 
-    return result
+        return result
+
+# We'd like to access ghcn.M.thing as a function, not a
+# bound method. This piece of code arranges that. Not
+# very elegantly.
+
+class Instance:
+    def __init__(self, **k):
+        self.__dict__.update(k)
+
+M = Instance(**M.__dict__)
